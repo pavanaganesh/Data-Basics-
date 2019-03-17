e9f125ec8b1f90d32c5a9a32e52d6d23a2e38359
pavanamg023 <39029804+pavanamg023@users.noreply.github.com>
2019-03-17 09:08:26 -0400

    Add files via upload

---
+++ hwdatabasics.m
@@ -0,0 +1,51 @@
+%% Data Basics
+% BY pavana Mysore Ganesh
+% Note: I was not able to add the path of bmes.dowloadurl and bmes.publish
+
+t = readtable('Immunotherapy.xlsx');
+% Changing the name of attributes
+attributes = {'sex','age','time','warts','type','area','diameter','result'};
+t.Properties.VariableNames=attributes;
+%% For each attribute, find its correlation with result of treatment
+for i = 1:numel(attributes)-1
+correlation(i) = corr(t{:,attributes{i}},t{:,attributes{end}});
+end
+m = table(attributes(1:end-1)',correlation');
+name = {'attributes','correlation'};
+m.Properties.VariableNames = name;
+disp(m)
+%% t-test and find statistically significant attribute
+
+testpos = t{:,'result'} == 1;
+testneg = t{:,'result'} == 0;
+for i = 1:numel(attributes)-1
+    [~,pvalue(i)] = ttest2(t{testpos,attributes{i}},t{testneg,attributes{i}});
+end
+name2 = {'attributes','pvalue'};
+s = table(attributes(1:end-1)',pvalue');
+s.Properties.VariableNames = name2;
+disp(s)
+%  Thershold Value is 0.05
+sig = attributes(pvalue<0.01);
+fprintf('statistically significant attribute is %s\n', char(sig))
+
+%% Report the correlation coefficients and the pvalues of the attributes in a tabular format.
+
+t1 = table(attributes(1:end-1)',correlation',pvalue');
+n3 = {'attributes','correlation','pvalue'};
+t1.Properties.VariableNames = n3;
+disp(t1)
+
+%% Box plot for significant attribute
+boxplot(t{:,'time'},t{:,'result'},'Labels',{'negative','positive'})
+xlabel('result');
+ylabel('time');
+
+
+
+
+
+
+
+
+
